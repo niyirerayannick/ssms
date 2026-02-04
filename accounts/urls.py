2131ff8a51ada2bb178579f1bad945336306b862
niyirerayannick <niyannick120@gmail.com>
2026-02-04 15:41:14 +0200

    db rwanda

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,4 +1,5 @@
 from django.urls import path
+from django.urls import reverse_lazy
 from django.contrib.auth import views as auth_views
 from . import views
 
@@ -11,14 +12,14 @@ urlpatterns = [
         template_name='registration/password_reset_form.html',
         email_template_name='registration/password_reset_email.html',
         subject_template_name='registration/password_reset_subject.txt',
-        success_url='/accounts/password-reset/done/'
+        success_url=reverse_lazy('accounts:password_reset_done')
     ), name='password_reset'),
     path('password-reset/done/', auth_views.PasswordResetDoneView.as_view(
         template_name='registration/password_reset_done.html'
     ), name='password_reset_done'),
     path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(
         template_name='registration/password_reset_confirm.html',
-        success_url='/accounts/reset/done/'
+        success_url=reverse_lazy('accounts:password_reset_complete')
     ), name='password_reset_confirm'),
     path('reset/done/', auth_views.PasswordResetCompleteView.as_view(
         template_name='registration/password_reset_complete.html'
